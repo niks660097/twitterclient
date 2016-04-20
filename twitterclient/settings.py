e7f59daad268df8dc3c9d7a2d72e9cd6679f6a46
Nirmal Singh <nirmal.singh@shadowfax.in>
2016-04-20 21:12:39 +0530

    sdalfj

--- twitterclient/settings.py
+++ twitterclient/settings.py
@@ -75,12 +75,10 @@ WSGI_APPLICATION = 'twitterclient.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+import dj_database_url
+DATABASES = {}
+
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Internationalization
